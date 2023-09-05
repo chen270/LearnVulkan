d79d5d89ce9a2f1432e870bdafdeae19b563661e
chen270 <chenchenfcj@163.com>
2023-09-05 17:51:30 +0800

    add shader realize code

--- 03_Shader/tools.hpp
+++ 03_Shader/tools.hpp
@@ -4,11 +4,20 @@
 #include <algorithm>
 #include <vector>
 #include <functional>
+#include <string>
+#include <fstream>
+#include <iostream>
 #include "vulkan/vulkan.hpp"
 
 using CreateSurfaceFunc = std::function<vk::SurfaceKHR(vk::Instance)>;
 
 namespace toy2d {
+
+#ifndef DIR_PATH
+#error shader path not define!
+#else
+#define S_PATH(str) DIR_PATH##str
+#endif
 
 template <typename T, typename U>
 void RemoveNosupportedElems(std::vector<T>& elems, const std::vector<U>& supportedElems,
@@ -25,6 +34,27 @@ void RemoveNosupportedElems(std::vector<T>& elems, const std::vector<U>& support
             i++;
         }
     }
+}
+
+static std::string ReadWholeFile(const std::string& filename) {
+    // spv 文件为二进制文件
+    // std::ios::binary 标志指示以二进制模式打开文件
+    // std::ios::ate 标志指示将文件指针移到文件末尾, 方便获取文件的大小。
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+    if (!file.is_open()) {
+        std::cout << "read " << filename << " failed" << std::endl;
+        return std::string{};
+    }
+
+    auto size = file.tellg();
+    std::string content;
+    content.resize(size);
+    file.seekg(0);
+
+    file.read(content.data(), content.size());
+
+    return content;
 }
 
 }
--- 03_Shader/toy2d.cpp
+++ 03_Shader/toy2d.cpp
@@ -1,15 +1,19 @@
 ﻿#include "toy2d.h"
 #include "context.h"
+#include "shader.hpp"
 
 namespace toy2d{
     void Init(const std::vector<const char*>& extensions, CreateSurfaceFunc func, const int w, const int h)
     {
         Context::Init(extensions, func);
         Context::GetInstance().InitSwapchain(w, h);
+        Shader::Init(ReadWholeFile(S_PATH("./bin/vert.spv")), ReadWholeFile(S_PATH("./bin/frag.spv")));
     }
 
     void Quit()
     {
+        Shader::Quit();
+
         // 需要先销毁 swapchain, 再销毁 device, 因为 swapchain 根据 device 创建的
         Context::GetInstance().DestroySwapchain();
 
