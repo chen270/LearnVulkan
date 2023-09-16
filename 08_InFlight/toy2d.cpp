8d3ee2fa79915aaa042b85fac4e8ef1194cfa520
chen70 <chenchenfcj@163.com>
2023-09-16 15:17:36 +0800

    add origincode

---
+++ 08_InFlight/toy2d.cpp
@@ -0,0 +1,34 @@
+﻿#include "toy2d.h"
+#include "context.h"
+#include "shader.hpp"
+
+namespace toy2d{
+    void Init(const std::vector<const char*>& extensions, CreateSurfaceFunc func, const int w, const int h)
+    {
+        Context::Init(extensions, func);
+        Context::GetInstance().InitSwapchain(w, h);
+        Shader::Init(ReadWholeFile(S_PATH("./bin/vert.spv")), ReadWholeFile(S_PATH("./bin/frag.spv")));
+        Context::GetInstance().m_renderProcess->InitLayout();
+        Context::GetInstance().m_renderProcess->InitRenderPass();
+        Context::GetInstance().m_swapchain->createFramebuffers(w, h);
+        Context::GetInstance().m_renderProcess->InitPipeline(w, h);
+        Context::GetInstance().InitRenderer();
+    }
+
+    void Quit()
+    {
+        Context::GetInstance().GetDevice().waitIdle(); // 让 cpu 等待所有操作完成
+        Context::GetInstance().DestroyRenderer();
+        Context::GetInstance().m_renderProcess.reset();
+        Shader::Quit();
+
+        // 需要先销毁 swapchain, 再销毁 device, 因为 swapchain 根据 device 创建的
+        Context::GetInstance().DestroySwapchain();
+
+        Context::Quit();
+    }
+
+    Renderer& GetRenderer() {
+        return *Context::GetInstance().m_renderer;
+    };
+}
---
+++ 08_InFlight/toy2d.h
@@ -0,0 +1,17 @@
+﻿#ifndef __TOY2D_H__
+#define __TOY2D_H__
+
+#include <vector>
+#include <functional>
+#include "vulkan/vulkan.hpp"
+#include "tools.hpp"
+#include "renderer.hpp"
+
+namespace toy2d
+{
+    void Init(const std::vector<const char*>& extensions, CreateSurfaceFunc func, const int w, const int h);
+    void Quit();
+    Renderer& GetRenderer();
+}
+
+#endif // __TOY2D_H__
